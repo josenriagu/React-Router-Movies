9ce9e4410e4cbcc999fb9d3ed83744528b55ec9c
Josemaria <josemarianriagu25@gmail.com>
2019-06-10 22:41:13 +0800

    Shrink some movie titles to prevent span overflow

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -11,12 +11,30 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <span className="saved-movie">{movie.title}</span>
-        ))}
+          <Link className='link' to={`/movies/${movie.id}`}>
+            {/* Shrink long movie titles */}
+            {
+              (movie.title === 'The Lord of the Rings: The Fellowship of the Ring')
+              ?
+              < span className="saved-movie">LOTR</span>
+              :
+              (movie.title === 'Terminator 2: Judgement Day')
+              ?
+              < span className="saved-movie">T2JDay</span>
+              :
+                (movie.title === 'Dumb and Dumber')
+              ?
+              < span className="saved-movie">D&D</span>
+              :
+              <span className="saved-movie">{movie.title}</span>
+            }
+          </Link>
+        ))
+        }
         <Link className='link' to={'/'}>
           <div className="home-button">Home</div>
         </Link>
-      </div>
+      </div >
     );
   }
 }

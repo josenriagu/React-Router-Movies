d1473e96c5f505311df3b97145c9fff6df58e0e0
Josemaria <josemarianriagu25@gmail.com>
2019-06-11 02:50:36 +0800

    Refactor into functional component Add unique key to child Add Clear button Adjust styles

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,40 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default class SavedList extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="saved-list">
-        <h3>Saved Movies:</h3>
-        {this.props.list.map(movie => (
-          <Link className='link' to={`/movies/${movie.id}`}>
-            {/* Shrink long movie titles */}
-            {
-              (movie.title === 'The Lord of the Rings: The Fellowship of the Ring')
+const SavedList = props => {
+  const clearStyle = (props.list.length !== 0) ? "home-button" : "hidden";
+  return (
+    <div className="saved-list">
+      <h3>Saved Movies:</h3>
+      {props.list.map(movie => (
+        <Link key={movie.id} className='link' to={`/movies/${movie.id}`}>
+          {/* Shrink long movie titles */}
+          {
+            (movie.title === 'The Lord of the Rings: The Fellowship of the Ring')
               ?
               < span className="saved-movie">LOTR</span>
               :
               (movie.title === 'Terminator 2: Judgement Day')
-              ?
-              < span className="saved-movie">T2JDay</span>
-              :
+                ?
+                < span className="saved-movie">T2JDay</span>
+                :
                 (movie.title === 'Dumb and Dumber')
-              ?
-              < span className="saved-movie">D&D</span>
-              :
-              <span className="saved-movie">{movie.title}</span>
-            }
-          </Link>
-        ))
-        }
-        <Link className='link' to={'/'}>
-          <div className="home-button">Home</div>
+                  ?
+                  < span className="saved-movie">D&D</span>
+                  :
+                  <span className="saved-movie">{movie.title}</span>
+          }
         </Link>
-      </div >
-    );
-  }
+      ))
+      }
+      <div
+        className={clearStyle}
+        onClick={props.clearSavedList}
+      >Clear</div>
+      <Link
+        className='link'
+        to={'/'}>
+        <div className="home-button">Home</div>
+      </Link>
+    </div >
+  );
 }
+
+export default SavedList;
